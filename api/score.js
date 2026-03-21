661ca221978b724018cbd9c9e2b75297efbb0810
Minato0630 <pandiyagokul@gmail.com>
2026-03-21 21:32:19 +0530

    mobile deploy

--- api/score.js
+++ api/score.js
@@ -1,26 +1,20 @@
 import { MongoClient } from "mongodb";
 
-const uri = process.env.MONGO_URI;
-const client = new MongoClient(uri);
+const client = new MongoClient(process.env.MONGO_URI);
 
 export default async function handler(req, res){
-
   await client.connect();
-  const db = client.db("ryukGame");
+  const db = client.db("RyukCluster");
   const col = db.collection("scores");
 
-  if(req.method === "POST"){
-    const { score } = req.body;
+  if(req.method==="POST"){
+    const {score} = req.body;
     await col.insertOne({score});
-    return res.status(200).json({msg:"saved"});
+    return res.json({msg:"saved"});
   }
 
-  if(req.method === "GET"){
-    const top = await col.find()
-      .sort({score:-1})
-      .limit(1)
-      .toArray();
-
+  if(req.method==="GET"){
+    const top = await col.find().sort({score:-1}).limit(1).toArray();
     return res.json(top[0] || {score:0});
   }
 }
